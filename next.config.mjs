305adb90af53cd3fe66ec59ce8dde84f7b050d52
sakshibhongle1221 <sakshibhongle1221@gmail.com>
2025-11-24 11:46:42 +0530

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
+ 
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://bikerapp-backend-694862036731.asia-south1.run.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
